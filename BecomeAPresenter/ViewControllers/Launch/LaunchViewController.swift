7cab4fc0073022cdc448a1b04acd877229ecd167
Hashaam Siddiq <hashaam83@gmail.com>
2020-08-03 15:39:50 +0400

    Embedded request camera authorization view

--- BecomeAPresenter/ViewControllers/Launch/LaunchViewController.swift
+++ BecomeAPresenter/ViewControllers/Launch/LaunchViewController.swift
@@ -12,8 +12,32 @@ class LaunchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupViews()
     }
 
+}
 
+private extension LaunchViewController {
+    func setupViews() {
+        addRequestCameraAuthorizationView()
+    }
+    
+    func addRequestCameraAuthorizationView() {
+        let requestCameraAuthorizationView = RequestCameraAuthorizationView()
+        requestCameraAuthorizationView.translatesAutoresizingMaskIntoConstraints = false
+        requestCameraAuthorizationView.delegate = self
+        view.addSubview(requestCameraAuthorizationView)
+        NSLayoutConstraint.activate([
+            requestCameraAuthorizationView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            requestCameraAuthorizationView.topAnchor.constraint(equalTo: view.topAnchor),
+            requestCameraAuthorizationView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            requestCameraAuthorizationView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension LaunchViewController: RequestCameraAuthorizationViewDelegate {
+    func requestCameraAuthorizationActionButtonTapped() {
+        print("request camera authorization button tapped")
+    }
 }
